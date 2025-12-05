d6c3733cf5bfc9437bba0bbadd735491a5ef3a0d
sdouglas2799-cyber <sdouglas2799@gmail.com>
2025-12-05 13:52:18 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,34 @@
-const CACHE_NAME = "trator-app-v1";
+const CACHE_NAME = "appcv-cache-v1";
 const FILES_TO_CACHE = [
   "./",
-  "./APPCV.html",
+  "./index.html",
   "./manifest.json",
   "./icone_192.png",
   "./icone_512.png"
 ];
 
-// INSTALAÇÃO
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-// ATIVAÇÃO
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
       );
     })
   );
   self.clients.claim();
 });
 
-// FETCH
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request).catch(() => caches.match("./index.html"));
     })
   );
 });
